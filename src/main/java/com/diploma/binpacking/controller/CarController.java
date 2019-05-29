b92c393ad45fb732399748d69df4b985dc1e07ef
Sirko2097 <2097_Cthusq>
2019-05-29 17:02:16 +0300

    Added CarService and CarController. For what? I don't know.

---
+++ src/main/java/com/diploma/binpacking/controller/CarController.java
@@ -0,0 +1,28 @@
+package com.diploma.binpacking.controller;
+
+import com.diploma.binpacking.model.Car;
+import com.diploma.binpacking.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cars")
+public class CarController {
+
+    private CarService carService;
+
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Car>> getAll() {
+        return ResponseEntity.ok(carService.getAll());
+    }
+}
