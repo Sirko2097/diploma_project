aa32a4c5c2000e9626eb43bd063ff714f616562b
Sirko2097 <2097_Cthusq>
2019-05-29 16:38:18 +0300

    Initial commit

---
+++ src/main/java/com/diploma/binpacking/BinpackingApplication.java
@@ -0,0 +1,13 @@
+package com.diploma.binpacking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinpackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinpackingApplication.class, args);
+	}
+
+}
